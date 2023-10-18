1b59e6f7ece2ea75c571ee099d0f9aaf84c4b76e
Saulius Anusas <saulius.anusas@gmail.com>
2023-10-18 18:44:22 +0300

    First function

---
+++ function1.py
@@ -0,0 +1,15 @@
+# 1 --------------------------------------------
+# def add_two_numbers(a, b):
+#     print(a+b)
+# add_two_numbers(5, 8)
+# add_two_numbers(50, 81)
+# add_two_numbers(500, 82134)
+
+# 2 --------------------------------------------
+
+# def add_two_numbers(a, b):
+#     return a + b
+
+# num_sum = add_two_numbers(12634, 32132565)
+
+# 3 --------------------------------------------
