e66566829d32e284061cf8fdc9f28db93875e4ef
Saulius Anusas <saulius.anusas@gmail.com>
2023-10-19 21:14:31 +0300

    Updaes and ideas

--- task1.py
+++ task1.py
@@ -1,35 +1,36 @@
-def do_add(a:int, b:int) -> float:
+from typing import Union
+
+def do_add(a:float, b:float) -> float:
     return float(a+b)
 
-def do_sub(a:int, b:int) -> float:
+def do_sub(a:float, b:float) -> float:
     return float(a-b)
 
-def do_mult(a:int, b:int) -> float:
+def do_mult(a:float, b:float) -> float:
     return float(a*b)
 
-def do_div(a:int, b:int) -> float:
+def do_div(a:float, b:float) -> float:
     return float(a/b)
 
-def mind_f(a:int, b:int, c:str) -> str:
+def mind_f(a:float, b:float, c:str) -> str:
     return str(a)+c+str(b)
 
-fisrts_num = int(input("Pleas enter a number 1: "))
-second_num = int(input("Pleas enter a number 2: "))
-simbol = input("Please enter simbols '-', '+', '*', '/': ")
+def check_execute(a:float, b:float, c: str):
+    if c == "-":
+        return print(do_sub(a, b))
+    elif c == "+":
+        return print(do_add(a, b))
+    elif c == "*":
+        return print(do_mult(a, b))
+    elif c == "/":
+        return print(do_div(a, b))
+    else:        
+        return print(f"What did you do?! {mind_f(a, b, c)}")
 
-while True:
-    if simbol == "-":
-        print(do_sub(fisrts_num, second_num))
-        break
-    elif simbol == "+":
-        print(do_add(fisrts_num, second_num))
-        break
-    elif simbol == "*":
-        print(do_mult(fisrts_num, second_num))
-        break
-    elif simbol == "/":
-        print(do_div(fisrts_num, second_num))
-        break
-    else:
-        print(f"What did you do?! {mind_f(fisrts_num, second_num, simbol)}")
-        break
+stop_cycle = True
+while stop_cycle:
+    first_number = float(input("Pleas enter a number 1: "))
+    second_number = float(input("Pleas enter a number 2: "))
+    simbol = input("Please enter simbols '-', '+', '*', '/': ")
+    
+    check_execute(first_number, second_number, simbol)
